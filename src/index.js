696037bfed5e50517dc3047ac9b5f720cbba802d
BebopTC <34389635+BebopTC@users.noreply.github.com>
2018-01-27 19:29:59 -0600

    Create stubs for RPC methods, test files, etc

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export * from './turtlecoin-walletd.js'
---
+++ src/turtlecoin-walletd.js
@@ -0,0 +1,62 @@
+export function reset() {
+}
+
+export function save() {
+}
+
+export function getViewKey() {
+}
+
+export function getSpendKeys() {
+}
+
+export function getStatus() {
+}
+
+export function getAddresses() {
+}
+
+export function createAddress() {
+}
+
+export function deleteAddress() {
+}
+
+export function getBalance() {
+}
+
+export function getBlockHashes() {
+}
+
+export function getTransactionHashes() {
+}
+
+export function getTransactions() {
+}
+
+export function getUnconfirmedTransactionHashes() {
+}
+
+export function getTransaction() {
+}
+
+export function sendTransaction() {
+}
+
+export function createDelayedTransaction() {
+}
+
+export function getDelayedTransactionHashes() {
+}
+
+export function deleteDelayedTransaction() {
+}
+
+export function sendDelayedTransaction() {
+}
+
+export function sendFusionTransaction() {
+}
+
+export function estimateFusion() {
+}
