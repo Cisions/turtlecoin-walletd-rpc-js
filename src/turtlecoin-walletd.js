a66b09063f3e9574290e73cd36fe2e08e51ecb45
BebopTC <34389635+BebopTC@users.noreply.github.com>
2018-01-29 16:13:53 -0600

    Implement more wallet methods

--- src/turtlecoin-walletd.js
+++ src/turtlecoin-walletd.js
@@ -1,62 +1,208 @@
-export function reset() {
+export function reset(id, viewSecretKey. xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "reset",
+    params  : {
+      viewSecretKey
+    }
+  }))
 }
 
-export function save() {
+export function save(id, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
-export function getViewKey() {
+export function getViewKey(id, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getViewKey"
+  }))
 }
 
-export function getSpendKeys() {
+export function getSpendKeys(id, address, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getSpendKeys,
+    params  : {
+      address
+    }
+  }))
 }
 
-export function getStatus() {
+export function getStatus(id, xhr) {
+
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getStatus"
+  }))
 }
 
-export function getAddresses() {
+export function getAddresses(id, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getAddresses"
+  }))
 }
 
-export function createAddress() {
+export function createAddress(id, secretSpendKey, publicSpendKey, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "createAddress",
+    params  : {
+      ...secretSpendKey && {secretSpendKey},
+      ...publicSpendKey && {publicSpendKey}
+    }
+  }))
 }
 
-export function deleteAddress() {
+export function deleteAddress(id, address, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "deleteAddress",
+    params  : {
+      address
+    }
+  }))
 }
 
-export function getBalance() {
+export function getBalance(id, address, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getBalance",
+    params  : {
+      address
+    }
+  }))
 }
 
-export function getBlockHashes() {
+export function getBlockHashes(id, firstBlockIndex, blockCount, xhr) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getBlockHashes",
+    params  : {
+      blockCount,
+      firstBlockIndex
+    }
+  }))
 }
 
-export function getTransactionHashes() {
+export function getTransactionHashes(
+  id,
+  blockCount,
+  firstBlockIndex,
+  blockHash,
+  paymentId,
+  addresses,
+  xhr
+) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getTransactionHashes"
+  }))
 }
 
-export function getTransactions() {
+export function getTransactionHashes(
+  id,
+  blockCount,
+  firstBlockIndex,
+  blockHash,
+  paymentId,
+  addresses,
+  xhr
+) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getTransactions"
+  }))
 }
 
-export function getUnconfirmedTransactionHashes() {
+export function getUnconfirmedTransactionHashes(
+  id,
+  addresses,
+  xhr
+) {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "getUnconfirmedTransactionHashes"
+  }))
 }
 
 export function getTransaction() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
 export function sendTransaction() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
 export function createDelayedTransaction() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
 export function getDelayedTransactionHashes() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
 export function deleteDelayedTransaction() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
 export function sendDelayedTransaction() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
 export function sendFusionTransaction() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
 
 export function estimateFusion() {
+  xhr.send(JSON.stringify({
+    jsonrpc : "2.0"
+    id,
+    method  : "save"
+  }))
 }
---
+++ src/turtlecoin-walletd.spec.js
@@ -0,0 +1,9 @@
+import { expect }       from 'chai'
+import { buildXHR }     from './xhr-utils'
+
+import * as walletd     from './turtlecoin-walletd'
+
+const host    = 'http://localhost',
+      port    = 8070
+
+
