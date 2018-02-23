f09f91f343ee93e15b06d8705bdbae762b8a287d
BebopTC <34389635+BebopTC@users.noreply.github.com>
2018-02-22 21:05:59 -0600

    Remove old unused dirty test spec

--- src/turtlecoin-walletd.spec.js
+++
@@ -1,9 +0,0 @@
-import { expect }       from 'chai'
-import { buildXHR }     from './xhr-utils'
-
-import * as walletd     from './turtlecoin-walletd'
-
-const host    = 'http://localhost',
-      port    = 8070
-
-
