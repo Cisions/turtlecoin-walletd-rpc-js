59a6eb69b3f1f011e91a1bb987c4e194f1914918
BebopTC <34389635+BebopTC@users.noreply.github.com>
2018-03-03 20:23:51 -0600

    Remove local XHR dep and cross fingers

--- src/buildXHR.js
+++ src/buildXHR.js
@@ -7,7 +7,7 @@ const buildXHR = (host, port, success, error) => {
   xhr.setRequestHeader("Accept", "application/json")
 
   xhr.onload = () => {
-    success(xhr.responseText)
+    success(JSON.parse(xhr.responseText)
   }
 
   xhr.onerror = (err) => {
